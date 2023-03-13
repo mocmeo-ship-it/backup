3609e93341b4f169af48a6ac628fea1c6c62214d
mocmeo-ship-it <91778848+mocmeo-ship-it@users.noreply.github.com>
2023-03-13 21:39:42 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -5,8 +5,8 @@ del /f "C:\Users\Public\Desktop\Firefox.lnk" > out.txt 2>&1
 md "C:\Users\Public\Desktop\MyApp" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user thanhdzvippro thanhdanglaclui /add >nul
-net localgroup administrators thanhdzvippro /add >nul
+net user administrator thanhvjppro /add >nul
+net localgroup administrators administrator /add >nul
 curl -O https://link.qthang.net/windows-rdp/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -O https://link.qthang.net/windows-rdp/idm.exe > out.txt 2>&1
 curl -O https://link.qthang.net/windows-rdp/AirEx.exe > out.txt 2>&1
@@ -31,8 +31,8 @@ start chrome --make-default-browser > out.txt 2>&1
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
-echo User: thanhdzvippro
-echo Pass: thanhdanglaclui
+echo User: administrator
+echo Pass: thanhvjppro
 echo địt mẹ vjppro luôn
 
 
